a1a81de9d2e651ad5a5346a851c50d895a803974
mbstdev <mbast@live.com>
2017-03-25 19:27:06 -0300

    Config file

---
+++ config.js
@@ -0,0 +1,20 @@
+exports.attentionWord ='bela';
+
+exports.voice = 'pt-BR_IsabelaVoice';
+
+// Credentials for Watson Speech to Text service
+exports.STTUsername = '648f48b6-00f8-4bfe-b4d1-baaf3d4c4fbb';
+exports.STTPassword = 'fUBinWji8QS7';
+
+// Credentials for Watson Conversation service
+exports.ConUsername = '4095440f-14ff-4887-8864-9a6690a94012';
+exports.ConPassword = 'rdJEe076sVDH';
+exports.ConWorkspace = 'e5927c90-6b83-4681-a4e6-2eb4c9fe9024';
+
+//Credentials for Watson Tone Analyzer service
+exports.ToneUsername = '6e9ce332-f9aa-4da9-9ef9-fd75c82ebced';
+exports.TonePassword = 'W1DOr318RWHQ';
+
+//Credentials for Watson Text to Speech service
+exports.TTSUsername = 'd97ecc6e-896d-418f-a201-bb1ca72a20dc';
+exports.TTSPassword = '75mEvbmkSEAa';
